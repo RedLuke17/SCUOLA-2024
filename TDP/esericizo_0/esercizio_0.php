77aa2d93b916881086405b952117628dafb63347
RedLuke17 <lukeishere17@gmail.com>
2023-09-28 15:56:57 +0200

    primi esercizi con php

---
+++ TDP/esericizo_0/esercizio_0.php
@@ -0,0 +1,11 @@
+<?php
+    echo "<b>Il mio primo SCRIPT -PHP-</b>
+    <br />
+    Il mio nome e' Luca
+    <br />
+    Il mio cognome e' Rossi
+    <br />
+    Vivo a Modugno
+    <br />
+    Provincia di BA";
+?>
---
+++ TDP/esericizo_0/esercizio_0_bis.php
@@ -0,0 +1,20 @@
+<?php
+    $A = 100;
+    $B = 300;
+    $C = 5.5;
+
+    echo "<b>Il mio secondo SCRIPT -PHP-</b>
+    <br />
+    <br />
+    Valore contenuto nella variabile <b>A</b>= $A 
+    <br />
+    Valore contenuto nella variabile <b>B</b>= $B
+    <br />
+    Valore contenuto nella variabile <b>C</b>= $C
+    <br />
+    <br />
+    <br />
+    Somma di <b>A</b> con <b>B</b>= ",$A+$B,"
+    <br />
+    Quoziente di <b>B</b> con <b>C</b>= ",$B/$C;
+?>
