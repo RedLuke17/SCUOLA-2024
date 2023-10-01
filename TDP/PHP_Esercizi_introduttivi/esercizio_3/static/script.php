c79e1219511ac225fdee652c8e25a0f8e994aab3
RedLuke17 <lukeishere17@gmail.com>
2023-10-01 14:03:11 +0200

    updated php

--- TDP/PHP_Esercizi_introduttivi/esercizio_3/static/script.php
+++ TDP/PHP_Esercizi_introduttivi/esercizio_3/static/script.php
@@ -19,24 +19,83 @@
         //quantitativo di partite giocate dalle squadre
         $partiteGiocate_1 = $numeroPartiteVinte_1 + $numeroPartitePareggiate_1 + $numeroPartitePerse_1;
         $partiteGiocate_2 = $numeroPartiteVinte_2 + $numeroPartitePareggiate_2 + $numeroPartitePerse_2;
+        $giocateSquadra;
         if ($partiteGiocate_1 == $partiteGiocate_2) {
-            echo "Le squadre hanno giocato lo stesso numero di partite<br/>";
+            $giocateSquadra = "Le due squadre hanno giocato lo stesso numero di partite<br/>";
         } else if ($partiteGiocate_1 < $partiteGiocate_2) {
-            echo "La squadra $nomeSquadra2 ha giocato piu partite della squadra $nomeSquadra1<br/>";
+            $giocateSquadra = "La squadra $nomeSquadra2 ha giocato piu partite della squadra $nomeSquadra1<br/>";
         } else if ($partiteGiocate_1 > $partiteGiocate_2) {
-            echo "La squadra $nomeSquadra1 ha giocato piu partite della squadra $nomeSquadra2<br/>";
+            $giocateSquadra = "La squadra $nomeSquadra1 ha giocato piu partite della squadra $nomeSquadra2<br/>";
         }
 
         //punteggio delle squadre
         $punteggioSquadra_1 = $numeroPartiteVinte_1 + $numeroPartitePareggiate_1 + $numeroPartitePerse_1;
         $punteggioSquadra_2 = $numeroPartiteVinte_2 + $numeroPartitePareggiate_2 + $numeroPartitePerse_2;
+        $nomeSquadraPunteggioAlto;
+        $nomeSquadraPunteggioBasso;
+        $punteggioSquadraAlto;
+        $punteggioSquadraBasso;
         if ($punteggioSquadra_1 == $punteggioSquadra_2) {
-            echo "Il punteggio delle squadre è uguale<br/>";
+            $nomeSquadraPunteggioAlto = $nomeSquadra1;
+            $punteggioSquadraAlto = $punteggioSquadra_1;
+
+            $nomeSquadraPunteggioBasso = $nomeSquadra2;
+            $punteggioSquadraBasso = $punteggioSquadra_2;
         } else if ($punteggioSquadra_1 < $punteggioSquadra_2) {
-            echo "Il punteggio della squadra $nomeSquadra2 e' il piu alto<br/>";
+            $nomeSquadraPunteggioAlto = $nomeSquadra2;
+            $punteggioSquadraAlto = $punteggioSquadra_2;
+
+            $nomeSquadraPunteggioBasso = $nomeSquadra1;
+            $punteggioSquadraBasso = $punteggioSquadra_1;
         } else if ($punteggioSquadra_1 > $punteggioSquadra_2) {
-            echo "Il punteggio della squadra $nomeSquadra1 e' il piu alto<br/>";
+            $nomeSquadraPunteggioAlto = $nomeSquadra1;
+            $punteggioSquadraAlto = $punteggioSquadra_1;
+
+            $nomeSquadraPunteggioBasso = $nomeSquadra2;
+            $punteggioSquadraBasso = $punteggioSquadra_2;
         }
+
+        echo "
+        <table border='1'>
+            <tr align='center'>
+                <td colspan='2'>Dati inseriti nel form</td>
+                <td>Output prodotto dal server</td>
+            </tr>
+            <tr align='center'>
+                <td>
+                    Nome <br>
+                    Vinte <br>
+                    Pareggiate <br>
+                    Perse
+                </td>
+                <td>
+                    $nomeSquadra1 <br>
+                    $numeroPartiteVinte_1 <br>
+                    $numeroPartitePareggiate_1 <br>
+                    $numeroPartitePerse_1
+                </td>
+                <td align='left' rowspan='2'>
+                    <b>Classifica</b> <br>
+                    $nomeSquadraPunteggioAlto = $punteggioSquadraAlto <br>
+                    $nomeSquadraPunteggioBasso = $punteggioSquadraBasso <br>
+                    $giocateSquadra
+                </td>
+            </tr>
+            <tr align='center'>
+                <td>
+                    Nome <br>
+                    Vinte <br>
+                    Pareggiate <br>
+                    Perse
+                </td>
+                <td>
+                    $nomeSquadra2 <br>
+                    $numeroPartiteVinte_2 <br>
+                    $numeroPartitePareggiate_2 <br>
+                    $numeroPartitePerse_2
+                </td>
+            </tr>
+        </table>";
     } else {
         echo "Devi obbligatoriamente compilare tutti i dati :)";
     }
