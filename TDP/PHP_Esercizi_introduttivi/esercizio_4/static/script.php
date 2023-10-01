c82fc6e7cb66bdeee950ee0c341e8231fa02e5b0
RedLuke17 <lukeishere17@gmail.com>
2023-10-01 14:22:48 +0200

    updated php

--- TDP/PHP_Esercizi_introduttivi/esercizio_4/static/script.php
+++ TDP/PHP_Esercizi_introduttivi/esercizio_4/static/script.php
@@ -4,18 +4,27 @@
     $nPorteHDMI = $_POST["nPorteHDMI"];
     $refreshrate = $_POST["refreshrate"];
     $risoluzione = $_POST["risoluzione"];
+    $consiglioHDMI;
+    $consiglioREFRESHRATE;
+    $consiglioRISOLUZIONE;
 
     if ($marca && $modello && $nPorteHDMI && $refreshrate && $risoluzione != "") {
         if ($nPorteHDMI < 2) {
-            echo "Ti consiglio di acquistare un televisore con almeno due porte hdmi<br>";
-        } 
+            $consiglioHDMI = "Ti consiglio di acquistare un televisore con almeno due porte hdmi";
+        } else {
+            $consiglioHDMI = "Porte HDMI: ok.";
+        }
         
         if($refreshrate < 50) {
-            echo "Ti consigliamo di acquistare un televisiore con un refreshrate maggiore di 50Hz<br>";
+            $consiglioREFRESHRATE = "Ti consigliamo di acquistare un televisiore con un refreshrate maggiore di 50Hz";
+        } else {
+            $consiglioREFRESHRATE = "Refresh rate: ok.";
         }
 
         if ($risoluzione == "HD Ready" || $risoluzione == "Full HD") {
-            echo "Ti consigliamo di comprare un televisore che abbia una risoluzione piu alta del Full HD<br>";
+            $consiglioRISOLUZIONE = "Ti consigliamo di comprare un televisore che abbia una risoluzione piu alta del Full HD";
+        } else {
+            $consiglioRISOLUZIONE = "Risoluzione: ok.";
         }
 
         if ($nPorteHDMI >= 2 && $refreshrate >= 50) {
@@ -23,6 +32,36 @@
                 echo "e' un buon televisore";
             }
         }
+
+        echo "
+        <table border='1'>
+            <tr align='center'>
+                <td colspan='2'>Dati inseriti nel form</td>
+                <td>Output prodotto dal server</td>
+            </tr>
+            <tr align='center'>
+                <td>
+                    Marca <br>
+                    Modello <br>
+                    Porte hdmi <br>
+                    Refresh rate <br>
+                    Risoluzione
+                </td>
+                <td>
+                    $marca <br>
+                    $modello <br>
+                    $nPorteHDMI <br>
+                    $refreshrate <br>
+                    $risoluzione
+                </td>
+                <td align='left'>
+                    <b>Consigli di acquisto</b> <br>
+                    $consiglioHDMI <br>
+                    $consiglioREFRESHRATE <br>
+                    $consiglioRISOLUZIONE
+                </td>
+            </tr>
+        </table>";
     } else {
         echo "Inserisci tutti i dati";
     }
