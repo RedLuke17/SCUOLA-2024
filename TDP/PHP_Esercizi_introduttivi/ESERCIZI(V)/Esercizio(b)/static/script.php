0f9c546c40682ceedc784bc5568b5ef66bbe319a
RedLuke17 <lukeishere17@gmail.com>
2023-10-29 16:57:22 +0100

    update

--- TDP/PHP_Esercizi_introduttivi/ESERCIZI(V)/Esercizio(b)/static/script.php
+++ TDP/PHP_Esercizi_introduttivi/ESERCIZI(V)/Esercizio(b)/static/script.php
@@ -1,18 +1,21 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $variabile = $_POST["primoNumero"];
-
-        $intero = (int)$variabile;
-        $float = (float)$variabile;
-        $boolean = filter_var($variabile);
-        $string = (string)$variabile;
-
-        echo "Trasformata in integer vale -->$intero
-        <br/>
-        Trasformata in float vale -->$float
-        <br/>
-        Trasformata in boolean vale -->",$boolean ? "true" : "false","
-        <br/>
-        Trasformata in string vale -->$string";
+        $a = $_POST["a"];
+        $b = $_POST["b"];
+        $c = $_POST["c"];
+        $equazione = ($b * $b) - (4 * $a * $c);
+        if ($equazione > 0) {
+            $radice1 = (-$b + sqrt($equazione)) / (2 * $a);
+            $radice2 = (-$b - sqrt($equazione)) / (2 * $a);
+            echo "L'equazione ha due soluzioni reali distinte:<br>";
+            echo "Radice 1: $radice1<br>";
+            echo "Radice 2: $radice2<br>";
+        } elseif ($equazione == 0) {
+            $radice1 = -$b / (2 * $a);
+            echo "L'equazione ha una soluzione reale (due soluzioni coincidenti):<br>";
+            echo "Radice unica: $radice1<br>";
+        } else {
+            echo "L'equazione non ammette alcuna soluzione reale.<br>";
+        }
     }
 ?>
