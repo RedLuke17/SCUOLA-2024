b3723dbe24214a87a1a4a0435ee506e5a236a28a
RedLuke17 <lukeishere17@gmail.com>
2023-11-08 17:14:34 +0100

    update

--- TDP/PHP_Esercizi_introduttivi/ESERCIZI(V)/Esercizio(b)/static/script.php
+++ TDP/PHP_Esercizi_introduttivi/ESERCIZI(V)/Esercizio(b)/static/script.php
@@ -3,14 +3,14 @@
         $a = $_POST["a"];
         $b = $_POST["b"];
         $c = $_POST["c"];
-        $equazione = ($b * $b) - (4 * $a * $c);
-        if ($equazione > 0) {
-            $radice1 = (-$b + sqrt($equazione)) / (2 * $a);
-            $radice2 = (-$b - sqrt($equazione)) / (2 * $a);
+        $delta = ($b * $b) - (4 * $a * $c);
+        if ($delta > 0) {
+            $radice1 = (-$b + sqrt($delta)) / (2 * $a);
+            $radice2 = (-$b - sqrt($delta)) / (2 * $a);
             echo "L'equazione ha due soluzioni reali distinte:<br>";
             echo "Radice 1: $radice1<br>";
             echo "Radice 2: $radice2<br>";
-        } elseif ($equazione == 0) {
+        } elseif ($delta == 0) {
             $radice1 = -$b / (2 * $a);
             echo "L'equazione ha una soluzione reale (due soluzioni coincidenti):<br>";
             echo "Radice unica: $radice1<br>";
