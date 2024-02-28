d303a7a118b7cd72a6d8378fa870ebd410730878
RedLuke17 <lukeishere17@gmail.com>
2024-02-28 17:02:07 +0100

    update

--- TDP/ESERCIZI_SOCKET/ESERCIZIO(1)/Client.java
+++ TDP/ESERCIZI_SOCKET/ESERCIZIO(1)/Client.java
@@ -3,18 +3,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
-    /*
-    public Client(String indirizzo, int port) {
-        try {
-            socket = new Socket(indirizzo, port);
-            System.out.println("connesso");
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         try {
             Socket socketClient = new Socket("localhost", 4999);
             InputStreamReader in = new InputStreamReader(socketClient.getInputStream());
